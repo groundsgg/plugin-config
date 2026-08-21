2715c3113a0e56da12a51f005297ecd6b1fc22ff
Lukas Jost <lukas.jost@outlook.com>
2026-08-21 14:41:23 +0200

    fix(release): publish plugin config image (#15)

--- velocity/build.gradle.kts
+++ velocity/build.gradle.kts
@@ -1,3 +1,19 @@
+import com.github.gmazzo.buildconfig.BuildConfigExtension
+import org.gradle.api.tasks.testing.Test
+
 plugins { id("gg.grounds.velocity-conventions") }
 
-dependencies { implementation(project(":common")) }
+configure<BuildConfigExtension> {
+    buildConfigFields.remove(buildConfigFields.getByName("VERSION"))
+    buildConfigField("String", "VERSION", "\"${rootProject.version}\"")
+}
+
+dependencies {
+    implementation(project(":common"))
+    testImplementation(kotlin("test"))
+}
+
+tasks.named<Test>("test") {
+    dependsOn("shadowJar")
+    systemProperty("releaseVersion", rootProject.version.toString())
+}
